8095a7f3d24a3dd38c88f7d6b9cdd5612cce72a2
JayyN <star14.jn@gmail.com>
2019-01-07 09:53:08 -0500

    I added a picture file to the drawable layout of my quiz app

--- app/src/main/java/com/pattersonhs/jaylinn/quizappinitials/HighScore.java
+++ app/src/main/java/com/pattersonhs/jaylinn/quizappinitials/HighScore.java
@@ -5,5 +5,21 @@ public class HighScore {
     String name;
     int rank;
 
+public HighScore(String newName, int newScore, int newRank){
+        name = newName;
+        score = newScore;
+        rank = newRank;
+    }
 
+    public String getName() {return name;}
+
+    public int getScore() {return score;}
+
+    public int getRank() {return rank;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void setScore(int score) {this.score = score;}
+
+    public void setRank(int rank) {this.rank = rank;}
 }
